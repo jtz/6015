68717fc4b40e4e4d1d63aa5f7a777e53e0e28678
jingtian <zoujingtian@gmail.com>
2021-06-11 23:03:55 -0700

    add 6015

---
+++ src/main/java/jingtian/demo5/demo6015/Demo6015Application.java
@@ -0,0 +1,13 @@
+package jingtian.demo5.demo6015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo6015Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo6015Application.class, args);
+	}
+
+}
---
+++ src/test/java/jingtian/demo5/demo6015/Demo6015ApplicationTests.java
@@ -0,0 +1,13 @@
+package jingtian.demo5.demo6015;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo6015ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
