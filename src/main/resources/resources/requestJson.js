32559349e3efaed58cce54e1b5d431d29475e22e
Jingtian <zoujingtian@gmail.com>
2021-06-14 22:35:13 -0700

    update

--- src/main/resources/resources/requestJson.js
+++ src/main/resources/resources/requestJson.js
@@ -1,51 +1,74 @@
-/*
-a. An event occurs (mouse click):
-i. Write the event handler
-ii. Create a XMLHttpRequest object
-iii. Write the code to create the request for your JSON data on
-the server (you must first upload it to the server using FTP)
-iv. Send the request to the server
-v. Process the returned JSON data using JavaScript
-vi. Update the page content
-1. The JSON data must be displayed in a HTML table.
- */
+const button = document.getElementById("button");
+const table = document.getElementById("table");
 
+// i. Write the event handler
+button.onclick = function() { getDegrees()};
 
-(function() {
+const getDegrees = () => {
 
     // const httpURL = 'http://localhost:8080/getjson';
     const httpURL = 'https://jingtian6015.azurewebsites.net/getjson';
 
-    // i. Write the event handler
-    document.getElementById("button").onclick = function() {
-        makeRequest(httpURL);
-    }
-
     // ii. Create a XMLHttpRequest object
     const httpRequest = new XMLHttpRequest();
-
-    // iii. Write the code to create the request for your JSON data on the server
-    function makeRequest(url) {
-        if (!httpRequest) {
-        alert('Exiting: Cannot create an XML HTTP instance');
+    if (!httpRequest) {
+        alert("Error: Cannot create an XML HTTP instance.");
         return false;
-        }
-        alert(url);
-        httpRequest.onreadystatechange = alertContents;
-        // iv. Send the request to the server
-        httpRequest.open('GET', url, true);
-        httpRequest.send();
     }
 
-    // v. Process the returned JSON data using JavaScript
-    function alertContents() {
+    // iii. Write code to create the request for the JSON data on the server
+    httpRequest.open('GET', httpURL, true);
+    httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
-                document.write(httpRequest.responseText);
+                const text = JSON.parse(httpRequest.responseText);
+                const toInsert = displayInTable(text);
+                button.insertAdjacentElement("beforebegin", toInsert);
+                button.style.display = "none";
+                table.style.display = "inline-block";
             } else {
-                alert('Error with request');
+                alert("Error: There was a problem with the request.");
             }
         }
+    };
+
+    // iv. Send the request to the server
+    httpRequest.send();
+};
+
+// v. Process the returned JSON data using JavaScript
+// vi. Display the JSON data in a HTML table
+const displayInTable = (text) => {
+
+    // Get the array
+    const degrees = text.my_college_degrees;
+    // Get the count of rows
+    const rowCount = degrees.length;
+    // Get the keys array
+    let keys = Object.keys(degrees[0].degree);
+    // Get the count of columns
+    const columnCount = keys.length;
+
+    // Create the header row
+    const headerRow = table.insertRow(-1);
+    for (let i = 0; i < columnCount; i++) {
+        // Create the header th element
+        let headerCell = document.createElement("TH");
+        // Write the columnName
+        headerCell.innerHTML = keys[i].toUpperCase();
+        // Append columnName to the header row
+        headerRow.appendChild(headerCell);
     }
-}
-)();
+
+    // Create the data rows
+    for (let i = 0; i < rowCount; i++) {
+        const dataRow = table.insertRow(-1);
+        for (let j = 0; j < columnCount; j++) {
+            let dataCell = dataRow.insertCell(-1);
+            dataCell.innerHTML = degrees[i].degree[keys[j]];
+            dataRow.appendChild(dataCell);
+        }
+    }
+
+    return table;
+};
